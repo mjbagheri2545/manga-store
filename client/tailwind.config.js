6bd2942bb395805a0790a5612def81f04ee68e44
MohammadJavad Bagheri <javadbagheri25452545@gmail.com>
2024-12-08 14:17:40 +0330

    project created and initialized

---
+++ client/tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+import daisyuiThemes from "daisyui/src/theming/themes";
+import colors from "tailwindcss/colors";
+
+import forms from "@tailwindcss/forms";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.violet,
+        dark: {
+          body: "#1a1a1a",
+          DEFAULT: "#242424",
+          pure: "#101010",
+        },
+        error: "#B00020",
+        success: "#008B00",
+        info: "#0ea5e9",
+        warning: "#ffd600",
+      },
+    },
+  },
+  plugins: [forms, daisyui],
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          ...daisyuiThemes["[data-theme=dark]"],
+          primary: colors.violet[600],
+          "primary-content": "fff",
+          error: "#B00020",
+          success: "#008B00",
+          info: "#0ea5e9",
+          warning: "#ffd600",
+        },
+      },
+    ],
+  },
+};
---
+++ client/vite.config.ts
@@ -0,0 +1,20 @@
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite";
+
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+});
