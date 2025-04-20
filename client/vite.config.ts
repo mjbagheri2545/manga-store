a7ccf30d686cbc8a8739694d44ff7ad99d9f22f9
MohammadJavad Bagheri <javadbagheri25452545@gmail.com>
2025-04-20 18:58:28 +0330

    fix issue in client prod.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   preview: {
-    host: "https://manga-store-1.onrender.com",
     port: 80,
   },
   plugins: [react()],
